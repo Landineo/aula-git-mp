819a651697e18e0a6034fad9f245aacf692bdf21
landineo <laandim.lucas@outlook.com>
2025-03-11 10:15:11 -0300

    Primeiro push

---
+++ src/main/java/media/portal/Main.java
@@ -0,0 +1,21 @@
+package media.portal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try{
+        Runtime run = Runtime.getRuntime();
+        int retval = run.exec("identify -verbose /stornext/PRIMARIO/WORK/HIRES/FOTOS/5020450.JPG").waitFor();
+
+        if(retval == 0){
+            System.out.println("finalizou corretamente");
+        } else {
+            System.out.println("retval = " + retval);
+        }
+    } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
